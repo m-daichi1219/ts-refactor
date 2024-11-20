726c47f1b28977730c527813e5da5ef7a83df528
m-daichi <matu9525@gmail.com>
2024-11-20 22:53:15 +0900

    ref 変数名を意図が明確になるよう修正

--- src/1/index.ts
+++ src/1/index.ts
@@ -21,26 +21,26 @@ interface Play {
   type: PlayType;
 }
 
-const amountFor = (perf: Performance, play: Play) => {
-  let thisAmount = 0;
+const amountFor = (performance: Performance, play: Play): number => {
+  let result = 0;
   switch (play.type) {
     case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
       }
       break;
     case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (performance.audience > 20) {
+        result += 10000 + 500 * (performance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * performance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 };
 
 const statement = (invoice: Invoice, plays: Plays) => {
