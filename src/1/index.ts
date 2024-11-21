3bd950d9afedc3d9569d1b836b755379e29b89cc
m-daichi <matu9525@gmail.com>
2024-11-21 23:37:11 +0900

    ref ループ分を分割して関数の抽出

--- src/1/index.ts
+++ src/1/index.ts
@@ -63,20 +63,26 @@ const volumeCreditsFor = (performance: Performance): number => {
   return result;
 };
 
-const statement = (invoice: Invoice, plays: Plays) => {
-  let totalAmount = 0;
+const totalVolumeCredits = (invoice: Invoice): number => {
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
     // ボリューム特定のポイントを加算
     volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
+};
+
+const statement = (invoice: Invoice, plays: Plays) => {
+  let totalAmount = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  for (let perf of invoice.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))}(${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
 
   return result;
 };
