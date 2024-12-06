b528342c9db74b46f7edb6c2638f89c74d6e7b25
m-daichi <matu9525@gmail.com>
2024-12-06 21:44:04 +0900

    debug 分割したstatementを実行できるように修正

--- src/1/statement.ts
+++ src/1/statement.ts
@@ -31,14 +31,28 @@ const renderHtml = (data: InvoiceAndPlay) => {
   result += '<table>\n';
   result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
   for (let perf of data.performances) {
-    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
     result += `<td>${usd(perf.amount)}</td><tr>\n`;
   }
   result += '</table>\n';
   result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
   result += `<p>You earned<em>${data.totalVolumeCredits}</em>credits</p>\n`;
+
+  return result;
 };
 
 const htmlStatement = (invoice: Invoice, plays: Plays) => {
   return renderHtml(createStatementData(invoice, plays));
 };
+
+import * as fs from 'fs';
+
+// JSONファイルの読み込み
+const invoice = JSON.parse(fs.readFileSync('./src/1/invoices.json', 'utf8'));
+const plays = JSON.parse(fs.readFileSync('./src/1/plays.json', 'utf8'));
+
+// renderPlainText
+console.log(statement(invoice[0], plays));
+
+// renderHtml
+console.log(htmlStatement(invoice[0], plays));
