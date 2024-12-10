88643df4e989ee90942e26a7cca344edc0e4db3d
m-daichi <matu9525@gmail.com>
2024-12-10 23:52:50 +0900

    add テストコードがある状態でリファクタリングを再実装する準備

---
+++ tests/index.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest';
+import { statement } from '../src/index';
+
+describe('statement', () => {
+  it('should return correct statement for given invoice and plays', () => {
+    const invoice = [
+      {
+        customer: 'BigCo',
+        performances: [
+          {
+            playID: 'hamlet',
+            audience: 55,
+          },
+          {
+            playID: 'as-like',
+            audience: 35,
+          },
+          {
+            playID: 'othello',
+            audience: 40,
+          },
+        ],
+      },
+    ];
+    const plays = {
+      hamlet: {
+        name: 'Hamlet',
+        type: 'tragedy',
+      },
+      'as-like': {
+        name: 'As You Like It',
+        type: 'comedy',
+      },
+      othello: {
+        name: 'Othello',
+        type: 'tragedy',
+      },
+    };
+    const expected = `Statement for BigCo
+Hamlet: $650.00(55 seats)
+As You Like It: $580.00(35 seats)
+Othello: $500.00(40 seats)
+Amount owed is $1,730.00
+You earned 47 credits\n`;
+
+    const result = statement(invoice[0], plays);
+    expect(result).toBe(expected);
+  });
+});
