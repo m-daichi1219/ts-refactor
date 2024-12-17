53b57e326969ae721ecf950e208c0484c4dd08c4
m-daichi <matu9525@gmail.com>
2024-12-17 20:01:47 +0900

    fix エラーが発生しないように型定義と写経のミスを修正

--- src/4/province.ts
+++ src/4/province.ts
@@ -1,4 +1,15 @@
-const sampleProvinceData = () => {
+interface ProvinceJSON {
+  name: string;
+  producers: {
+    name: string;
+    cost: number;
+    production: number;
+  }[];
+  demand: number;
+  price: number;
+}
+
+const sampleProvinceData = (): ProvinceJSON => {
   return {
     name: 'Asia',
     producers: [
@@ -24,7 +35,19 @@ class Producer {
     this._production = data.production || 0;
   }
 
-  get production() {
+  get name(): string {
+    return this._name;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  set cost(arg: string) {
+    this._cost = parseInt(arg);
+  }
+
+  get production(): number {
     return this._production;
   }
 
@@ -36,51 +59,18 @@ class Producer {
   }
 }
 
-class Producer {
+class Province {
   private _name: string;
   private _producers: Producer[];
   private _totalProduction: number;
   private _demand: number;
   private _price: number;
 
-  get name() {
-    return this._name;
-  }
-  get producers() {
-    return this._producers.slice();
-  }
-  get totalProduction() {
-    return this._totalProduction;
-  }
-  set totalProduction(arg) {
-    this._totalProduction = arg;
-  }
-  get demand() {
-    this._demand;
-  }
-  set demand(arg) {
-    this._demand = parseInt(arg);
-  }
-  get price() {
-    return this._price;
-  }
-  set price(arg) {
-    this._price = parseInt(arg);
-  }
-}
-
-class Province {
-  private _name: string;
-  private _producers: Producer[];
-  private _totalProduction: number;
-  private __demand: number;
-  private _price: number;
-
-  constructor(doc: any) {
+  constructor(doc: ProvinceJSON) {
     this._name = doc.name;
     this._producers = [];
     this._totalProduction = 0;
-    this.__demand = doc.demand;
+    this._demand = doc.demand;
     this._price = doc.price;
     doc.producers.forEach((d) => this.addProducer(new Producer(this, d)));
   }
@@ -99,23 +89,56 @@ class Province {
   }
 
   get demandCost() {
-    let remainingDemand = this;
+    let remainingDemand = this.demand;
     let result = 0;
-    this._producers.sort((a,b) => a.cost - b.cost))
-    .forEach((p) => {
-      const contribution = Math.min(remainingDemand, p.production);
-      remainingDemand -= contribution;
-      result += contribution * p.cost;
-    });
+    this._producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((p) => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
 
-    return result
+    return result;
   }
 
-    get demandValue() {
-        return this.satisfiedDemand * this._price;
-    }
+  get demandValue() {
+    return this.satisfiedDemand * this._price;
+  }
 
-    get satisfiedDemand() {
-        return Math.min(this._demand, this.totalProduction);
-    }
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get producers() {
+    return this._producers.slice();
+  }
+
+  get totalProduction() {
+    return this._totalProduction;
+  }
+
+  set totalProduction(arg) {
+    this._totalProduction = arg;
+  }
+
+  get demand() {
+    return this._demand;
+  }
+
+  set demand(arg: any) {
+    this._demand = parseInt(arg);
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(arg: any) {
+    this._price = parseInt(arg);
+  }
 }
