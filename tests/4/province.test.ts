8cd6b60182cb95d79e5710224fe7a4d92260b5c5
m-daichi <matu9525@gmail.com>
2025-01-02 22:00:22 +0900

    fix テストコードの重複排除

--- tests/4/province.test.ts
+++ tests/4/province.test.ts
@@ -1,13 +1,15 @@
-import { expect, it, describe } from 'vitest';
+import { expect, it, describe, beforeEach } from 'vitest';
 import { Province, sampleProvinceData } from '../../src/4/province';
 
 describe('province', () => {
+  let asia: Province;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
   it('shortfall', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
   it('profit', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
   });
 });
