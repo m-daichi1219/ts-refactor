4c4ab436db049d01a2ca5060c5d531823b2d9c0d
m-daichi <matu9525@gmail.com>
2025-01-13 17:24:41 +0900

    add 自身のリファクタリングのメモ

--- src/6/inline2.js
+++ src/6/inline2.js
@@ -9,6 +9,18 @@ const refGetRating = (driver) => {
   return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 };
 
+/////////
+/** 三項演算子よりこっちのほうが分かりやすくね？ */
+const myRating = (driver) => {
+  if (myMoreThanFiveLateDeliveries(driver)) return 2;
+  return 1;
+};
+
+const myMoreThanFiveLateDeliveries = (driver) =>
+  driver.numberOfLateDeliveries > 5;
+
+//////////
+
 const reportLines = (aCustomer) => {
   const lines = [];
 
