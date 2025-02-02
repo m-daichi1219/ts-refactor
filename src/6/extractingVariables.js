49e6edb1aaa749f324b0fec1a466b4f82ebd39ed
m-daichi <matu9525@gmail.com>
2025-02-02 21:22:39 +0900

    add Bookクラスと予約管理ロジックを追加

--- src/6/extractingVariables.js
+++ src/6/extractingVariables.js
@@ -43,3 +43,23 @@ const example2 = () => {
   //   return basePrice > 1000;
   return anOrder.basePrice > 1000;
 };
+
+/** ---- */
+
+class Book {
+  addReservation(customer) {
+    this.zz_addReservation(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority === false);
+    this._reservations.push(customer);
+  }
+}
+
+/** --- */
+const inNewEngland = (aCustomer) => {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+};
+
+const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
