ee72503baf1337c6513c8e85670fa968fc00e060
m-daichi <matu9525@gmail.com>
2025-02-02 21:05:20 +0900

    add Orderクラスと価格計算ロジックを追加

---
+++ src/6/extractingVariables.js
@@ -0,0 +1,37 @@
+const example = () => {
+  return (
+    order.quantity * order.itemPrice -
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  );
+};
+
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+}
