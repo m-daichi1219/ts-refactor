de4fa7bd017fd3fc402c4f17a0fcf8714f5bf9c9
m-daichi <matu9525@gmail.com>
2025-02-02 20:53:49 +0900

    add ドライバーの遅延配達数に基づく評価関数を追加

--- src/6/funcInline.js
+++ src/6/funcInline.js
@@ -5,3 +5,7 @@ const getRating = (driver) => {
 const moreThanFiveLateDeliveries = (driver) => {
   return driver.numberOfLateDeliveries > 5;
 };
+
+const refGetRating = (driver) => {
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
+};
