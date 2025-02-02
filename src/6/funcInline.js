140bc0632f639a576ab2771cb33fe26729df813d
m-daichi <matu9525@gmail.com>
2025-02-02 20:58:40 +0900

    add 顧客データを収集する関数を追加

--- src/6/funcInline.js
+++ src/6/funcInline.js
@@ -9,3 +9,23 @@ const moreThanFiveLateDeliveries = (driver) => {
 const refGetRating = (driver) => {
   return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 };
+
+/** ------ */
+
+const reportLines = (aCustomer) => {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+};
+
+const gatherCustomerData = (out, aCustomer) => {
+  out.push(['name', aCustomer.name]);
+  out.push(['location', aCustomer.location]);
+};
+
+const refReportLines = (aCustomer) => {
+  const lines = [];
+  lines.push(['name', aCustomer.name]);
+  lines.push(['location', aCustomer.location]);
+  return lines;
+};
