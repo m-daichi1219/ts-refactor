0492c5d126d8ca0b0fb3cfc8cda4ac1c9327435b
m-daichi <matu9525@gmail.com>
2025-02-12 23:23:15 +0900

    Organizationクラスを追加し、データのカプセル化を実装

--- src/7/encapsulateRecord.js
+++ src/7/encapsulateRecord.js
@@ -8,3 +8,32 @@ const getRawDataOfOrganization = () => {
 result += `<h1>${getRawDataOfOrganization().name}</h1>`;
 // organization.name = newName;
 getRawDataOfOrganization().name = newName;
+
+/** ----- */
+
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+
+  set name(aString) {
+    this._data.name = aString;
+  }
+
+  get name() {
+    return this._data.name;
+  }
+}
+
+const organization2 = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
+
+const getRawDataOfOrganization2 = () => {
+  return organization2._data;
+};
+
+const getOrganization = () => {
+  return organization2;
+};
