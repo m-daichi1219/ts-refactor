58f9a8e12e1cc26f1467ea9372ae1acbe68b4c68
m-daichi <matu9525@gmail.com>
2025-02-17 22:57:03 +0900

    PersonおよびTelephoneNumberクラスを追加し、電話番号の管理機能を実装

---
+++ src/7/extractClass.js
@@ -0,0 +1,87 @@
+class Person {
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    this._name = arg;
+  }
+
+  get telephoneNumber() {
+    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+  }
+
+  get officeAreaCode() {
+    return this._officeAreaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this._officeAreaCode = arg;
+  }
+
+  get officeNumber() {
+    return this._officeNumber;
+  }
+
+  set officeNumber(arg) {
+    this._officeNumber = arg;
+  }
+}
+
+/** ------ */
+
+class TelephoneNumber {
+  get areaCode() {
+    return this._areaCode;
+  }
+
+  set areaCode(arg) {
+    this._areaCode = arg;
+  }
+
+  get number() {
+    return this._officeNumber;
+  }
+
+  set number(arg) {
+    this._officeNumber = arg;
+  }
+
+  get toString() {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+}
+
+class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    this._name = arg;
+  }
+
+  get telephoneNumber() {
+    return this._telephoneNumber.toString;
+  }
+
+  get officeAreaCode() {
+    return this._telephoneNumber.areaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this._telephoneNumber.areaCode = arg;
+  }
+
+  get officeNumber() {
+    return this._telephoneNumber.number;
+  }
+
+  set officeNumber(arg) {
+    this._telephoneNumber.number = arg;
+  }
+}
