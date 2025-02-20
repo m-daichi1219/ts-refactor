c62399668ef578f9175d68aaeefc76521eb73686
m-daichi <matu9525@gmail.com>
2025-02-20 23:45:56 +0900

    TrackingInformationおよびShipmentクラスを追加し、配送情報の管理機能を実装

---
+++ src/7/inlineClass.js
@@ -0,0 +1,49 @@
+class TrackingInformation {
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+  set shippingCompany(arg) {
+    this._shippingCompany = arg;
+  }
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+  get display() {
+    return `${this.shippingCompany}:${this.trackingNumber}`;
+  }
+}
+
+class Shipment {
+  get trackingInfo() {
+    // return this._trackingInformation.display;
+    return `${this.shippingCompany}:${this.trackingNumber}`;
+  }
+  get trackingInformation() {
+    return this._trackingInformation;
+  }
+  set trackingInformation(aTrackingInformation) {
+    this._trackingInformation = aTrackingInformation;
+  }
+  set shippingCompany(arg) {
+    this._trackingInformation.shippingCompany = arg;
+  }
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+  set shippingCompany(arg) {
+    this._shippingCompany = arg;
+  }
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+}
+
+// caller
+// aShipment.trackingInformation.shippingCompany = request.vendor;
+aShipment.shippingCompany = request.vendor;
